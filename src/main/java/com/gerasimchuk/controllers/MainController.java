a4962734cc4ae9805e63a4cfb4d7bfd20d4ed5ca
Reaper <reaper-16.17@mail.ru>
2018-09-03 09:12:35 +0300

    #2 created entities and repositories

--- src/main/java/com/gerasimchuk/controllers/MainController.java
+++ src/main/java/com/gerasimchuk/controllers/MainController.java
@@ -1,5 +1,9 @@
 package com.gerasimchuk.controllers;
 
+import com.gerasimchuk.entities.Manager;
+import com.gerasimchuk.repositories.CityRepository;
+import com.gerasimchuk.repositories.ManagerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,11 +11,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class MainController {
 
+    private CityRepository cityRepository;
     private static final org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(MainController.class);
+
+    @Autowired
+    public MainController(CityRepository cityRepository) {
+        this.cityRepository = cityRepository;
+    }
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index() {
+        cityRepository.create("City1", true);
         log.info("In method index of main controller");
+
         return "/login/login";
     }
 }
