b7a43b2d22e17749687729bbb3272ac47f22f49c
Reaper <reaper-16.17@mail.ru>
2018-10-18 21:09:58 +0300

    #17 added junit tests for RouteServiceImpl class

--- src/main/java/com/gerasimchuk/services/interfaces/RouteService.java
+++ src/main/java/com/gerasimchuk/services/interfaces/RouteService.java
@@ -1,9 +1,13 @@
 package com.gerasimchuk.services.interfaces;
 
 import com.gerasimchuk.dto.RouteDTO;
+import com.gerasimchuk.entities.Route;
+import com.gerasimchuk.utils.ReturnValuesContainer;
 
 public interface RouteService {
     boolean createRoute(RouteDTO routeDTO);
+    ReturnValuesContainer<Route> createRoute(RouteDTO routeDTO, int val);
     boolean updateRoute(RouteDTO routeDTO);
+    ReturnValuesContainer<Route>  updateRoute(RouteDTO routeDTO, int val);
     boolean deleteRoute(int routeId) throws Exception;
 }
